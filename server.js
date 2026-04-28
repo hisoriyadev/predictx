1ce5b49d1673158f4253e62b3e7b33b234fbd637
Hisoriya Dev <hisoriyad@gmail.com>
2026-04-28 14:52:32 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
